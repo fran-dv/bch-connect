307c1b3881874737daf8f02ad45f92474d64125e
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-09-30 23:59:35 -0300

    feat: implement WalletConnectContext

---
+++ src/contexts/WalletConnectContext.ts
@@ -0,0 +1,33 @@
+import type { Configuration } from "@/config/config";
+import type { UniversalConnector } from "@reown/appkit-universal-connector";
+import { createContext, useContext } from "react";
+
+export type ConnectWalletCallback = () => Promise<void>;
+export type DisconnectWalletCallback = () => Promise<void>;
+
+export interface WalletConnectContext {
+  config: Configuration;
+  session: UniversalConnector["provider"]["session"] | null;
+  provider: UniversalConnector["provider"] | null;
+  isConnecting: boolean;
+  isDisconnecting: boolean;
+  connectError: Error | null;
+  disconnectError: Error | null;
+  connect: ConnectWalletCallback;
+  disconnect: DisconnectWalletCallback;
+}
+
+export const WalletConnectContext = createContext<WalletConnectContext | null>(
+  null,
+);
+
+export const useWalletConnectContext = (): WalletConnectContext => {
+  const context = useContext(WalletConnectContext);
+
+  if (!context) {
+    throw new Error(
+      "useWalletConnectContext must be used within a WalletContextProvider",
+    );
+  }
+  return context;
+};
