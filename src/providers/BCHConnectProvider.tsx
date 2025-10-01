90cd583535e79c6ad07a9b3ed12ef033ec1b5232
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-10-01 00:01:29 -0300

    feat: implement BCHConnectProvider

---
+++ src/providers/BCHConnectProvider.tsx
@@ -0,0 +1,117 @@
+import { useEffect, useState } from "react";
+import { UniversalConnector } from "@reown/appkit-universal-connector";
+import { getUniversalConnector, type Configuration } from "@/config/config";
+import { WalletConnectContext } from "@/contexts/WalletConnectContext";
+
+interface Props {
+  children: React.ReactNode;
+  config: Configuration;
+}
+
+export const BCHConnectProvider: React.FC<Props> = ({
+  children,
+  config,
+}: Props) => {
+  const [universalConnector, setUniversalConnector] =
+    useState<UniversalConnector | null>(null);
+  const [session, setSession] = useState<
+    UniversalConnector["provider"]["session"] | null
+  >(null);
+  const [isConnecting, setIsConnecting] = useState(false);
+  const [connectError, setConnectError] = useState<Error | null>(null);
+  const [isDisconnecting, setIsDisconnecting] = useState(false);
+  const [disconnectError, setDisconnectError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    const provider = universalConnector?.provider;
+    if (!provider) return;
+
+    const handleConnect = async (connectInfo: {
+      session: UniversalConnector["provider"]["session"];
+    }) => {
+      setSession(connectInfo.session);
+    };
+
+    const handleDisconnect = async () => {
+      setSession(null);
+    };
+
+    provider.on("connect", handleConnect);
+    provider.on("disconnect", handleDisconnect);
+
+    return () => {
+      provider.removeListener("connect", handleConnect);
+      provider.removeListener("disconnect", handleDisconnect);
+    };
+  }, [universalConnector?.provider]);
+
+  useEffect(() => {
+    if (universalConnector) return;
+
+    (async () => {
+      try {
+        const universalConnector = await getUniversalConnector(config);
+        setUniversalConnector(universalConnector);
+      } catch (err) {
+        setConnectError(err as Error);
+      }
+    })();
+  }, [universalConnector, config]);
+
+  useEffect(() => {
+    const currentSession = universalConnector?.provider.session;
+
+    if (!currentSession) setSession(null);
+
+    setSession(currentSession);
+  }, [universalConnector?.provider]);
+
+  const connect = async () => {
+    if (!universalConnector) return;
+
+    setIsConnecting(true);
+    try {
+      await universalConnector.connect();
+    } catch (err) {
+      setConnectError(err as Error);
+    } finally {
+      setIsConnecting(false);
+    }
+  };
+
+  const disconnect = async () => {
+    const provider = universalConnector?.provider;
+
+    if (!provider) return;
+
+    setIsDisconnecting(true);
+    try {
+      await provider.disconnect();
+      setSession(null);
+    } catch (err) {
+      setDisconnectError(err as Error);
+    } finally {
+      setIsDisconnecting(false);
+    }
+  };
+
+  const walletConnectContextVaues: WalletConnectContext = {
+    config,
+    session,
+    provider: universalConnector?.provider ?? null,
+    isConnecting,
+    connectError,
+    isDisconnecting,
+    disconnectError,
+    connect,
+    disconnect,
+  };
+
+  return (
+    <WalletConnectContext.Provider value={walletConnectContextVaues}>
+      {children}
+    </WalletConnectContext.Provider>
+  );
+};
+
+export default BCHConnectProvider;
