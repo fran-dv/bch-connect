e0a89ff4fdc79aa495b5e73bfb7ea0a168e96be2
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-10-01 00:04:49 -0300

    feat: implement useWallet

--- src/hooks/useWallet.ts
+++ src/hooks/useWallet.ts
@@ -1,135 +1,102 @@
-import { BCHMethods, NetworksIds } from "@/config/config";
 import {
   useWalletConnectContext,
   type ConnectWalletCallback,
   type DisconnectWalletCallback,
-} from "@/context/WalletConnectContext";
-import { useEffect, useState } from "react";
-import {
-  type WcSignMessageRequest,
-  type WcSignMessageResponse,
-  type WcSignTransactionRequest,
-  type WcSignTransactionResponse,
-} from "@bch-wc2/interfaces";
-import { stringify } from "@bitauth/libauth";
+} from "@/contexts/WalletConnectContext";
+import addressToTokenAddress from "@/utils/addressToTokenAddress";
+import { useCallback, useEffect, useState } from "react";
+import useGetAddresses from "@/hooks/useGetAddresses";
 
 export interface UseWalletReturnType {
-  addresses: string[] | null;
-
+  address: string | null;
+  tokenAddress: string | null;
+  areAddressesLoading: boolean;
+  addressError: Error | null;
   isConnected: boolean;
   isConnecting: boolean;
-
+  connectError: Error | null;
+  isDisconnecting: boolean;
+  disconnectError: Error | null;
+  isError: boolean;
   connect: ConnectWalletCallback;
   disconnect: DisconnectWalletCallback;
-  signTransaction: (
-    options: WcSignTransactionRequest,
-  ) => Promise<WcSignTransactionResponse | undefined>;
-  signMessage: (
-    options: WcSignMessageRequest,
-  ) => Promise<WcSignMessageResponse | undefined>;
+  refetchAddresses: () => Promise<void>;
 }
 
 export const useWallet = (): UseWalletReturnType => {
-  const { connect, disconnect, session, provider, config } =
-    useWalletConnectContext();
+  const {
+    connect,
+    disconnect,
+    session,
+    provider,
+    config,
+    isConnecting,
+    connectError,
+    isDisconnecting,
+    disconnectError,
+  } = useWalletConnectContext();
+  const { getAddresses } = useGetAddresses();
   const isConnected = !!session;
-  const isConnecting = false;
-  const [addresses, setAddresses] = useState<string[] | null>(null);
+  const [address, setAddress] = useState<string | null>(null);
+  const [areAddressesLoading, setAreAddressesLoading] = useState(false);
+  const [addressError, setAddressError] = useState<Error | null>(null);
+  const [tokenAddress, setTokenAddress] = useState<string | null>(null);
+  const isError = !!addressError || !!connectError || !!disconnectError;
+
+  const fetchAddresses = useCallback(async () => {
+    setAreAddressesLoading(true);
+    try {
+      const addresses = await getAddresses();
+
+      if (!addresses) return;
+
+      setAddress(addresses[0]);
+      setAddressError(null);
+    } catch (err) {
+      setAddressError(err as Error);
+    } finally {
+      setAreAddressesLoading(false);
+    }
+  }, [getAddresses]);
 
   useEffect(() => {
     if (!isConnected || !session || !provider) {
-      setAddresses(null);
+      setAddress(null);
       return;
     }
 
-    const fetchAndSetAddresses = async () => {
-      try {
-        const addresses = await provider?.client.request<string[]>({
-          chainId: NetworksIds[config.network],
-          topic: session.topic,
-          request: {
-            method: BCHMethods.getAddresses,
-            params: {},
-          },
-        });
+    fetchAddresses();
+  }, [session, isConnected, provider, fetchAddresses, config.network]);
 
-        if (!addresses) return;
-
-        console.log("Addresses:", addresses);
-
-        setAddresses(addresses);
-      } catch (err) {
-        console.error("Failed to get addresses:", err);
-      }
-    };
-
-    fetchAndSetAddresses();
-  }, [session, provider, isConnected, config.network]);
-
-  const signTransaction = async (
-    options: WcSignTransactionRequest,
-  ): Promise<WcSignTransactionResponse | undefined> => {
-    if (!provider || !session) {
-      console.error(
-        "Error signing transactions: Provider or session is not defined",
-      );
-      return undefined;
-    }
+  useEffect(() => {
+    if (!address) return;
 
     try {
-      const response = await provider.client.request<WcSignTransactionResponse>(
-        {
-          chainId: NetworksIds[config.network],
-          topic: session.topic,
-          request: {
-            method: BCHMethods.signTransaction,
-            params: JSON.parse(stringify(options)),
-          },
-        },
-      );
-
-      return response;
-    } catch (err) {
-      console.error("Failed to sign transaction:", err);
-      return undefined;
-    }
-  };
-
-  const signMessage = async (
-    options: WcSignMessageRequest,
-  ): Promise<WcSignMessageResponse | undefined> => {
-    if (!provider || !session) {
-      console.error(
-        "Error signing messages: Provider or session is not defined",
-      );
-      return undefined;
-    }
-
-    try {
-      const response = await provider.client.request<WcSignMessageResponse>({
-        chainId: NetworksIds[config.network],
-        topic: session.topic,
-        request: {
-          method: BCHMethods.signMessage,
-          params: JSON.parse(stringify(options)),
-        },
+      const tokenAddress = addressToTokenAddress({
+        address,
+        network: config.network,
       });
-
-      return response;
+      setTokenAddress(tokenAddress);
+      setAddressError(null);
     } catch (err) {
-      console.error("Failed to sign message:", err);
-      return undefined;
+      setAddressError(err as Error);
     }
-  };
+  }, [address, config.network]);
 
   return {
-    addresses,
+    address,
+    tokenAddress,
+    areAddressesLoading,
+    addressError,
     isConnected,
     isConnecting,
+    connectError,
+    isDisconnecting,
+    disconnectError,
+    isError,
     connect,
     disconnect,
-    signTransaction,
-    signMessage,
+    refetchAddresses: fetchAddresses,
   };
 };
 
