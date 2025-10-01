77f3a85cb0287f5529d9ae9f6f18a0f92e5b3589
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-10-01 00:07:33 -0300

    feat: expose public API of the library

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export * from "@/hooks/useGetAddresses";
+export * from "@/hooks/useSignMessage";
+export * from "@/hooks/useSignTransaction";
+export * from "@/hooks/useWallet";
+export * from "@/providers/BCHConnectProvider";
+export { type Configuration, createConfig } from "@/config/config";
