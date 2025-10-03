5d17a937715ccc12dc9ce8e883ec63cabc671089
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-10-03 10:59:57 -0300

    feat(examples): add Background component

---
+++ examples/src/components/Background/Background.tsx
@@ -0,0 +1,17 @@
+import { SquaresBackground } from "./components/SquaresBackground";
+
+export const Background: React.FC = () => {
+  return (
+    <>
+      <SquaresBackground
+        speed={0.1}
+        squareSize={60}
+        direction="diagonal"
+        borderColor="gray"
+        hoverFillColor="rgba(10, 193, 142, 0.3)"
+      />
+    </>
+  );
+};
+
+export default Background;
