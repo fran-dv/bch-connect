b90954e700a8bc4b0448eab069f236eed42f652a
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-10-03 11:27:48 -0300

    feat(examples): add SubmitButton component

---
+++ examples/src/components/forms/SubmitButton/SubmitButton.tsx
@@ -0,0 +1,27 @@
+import clsx from "clsx";
+
+interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export const SubmitButton: React.FC<Props> = ({
+  children,
+  className,
+  ...rest
+}: Props) => {
+  return (
+    <button
+      type="submit"
+      className={clsx(
+        "flex items-center gap-2 text-white-bch bg-green-bch w-full px-4 py-2 rounded-full justify-center text-lg font-bold uppercase",
+        className,
+      )}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default SubmitButton;
