46aeaad1ef5d41479bf612554e1f9dc9601ee767
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-10-03 11:26:58 -0300

    feat(examples): add CustomForm and ControlledInput react-hook-form components

---
+++ examples/src/components/forms/CustomForm/CustomForm.tsx
@@ -0,0 +1,35 @@
+import clsx from "clsx";
+import {
+  FormProvider,
+  type FieldValues,
+  type SubmitHandler,
+  type UseFormReturn,
+} from "react-hook-form";
+
+interface Props<T extends FieldValues>
+  extends Omit<React.FormHTMLAttributes<HTMLFormElement>, "onSubmit"> {
+  methods: UseFormReturn<T>;
+  onSubmit: SubmitHandler<T>;
+  children: React.ReactNode;
+  className?: string;
+}
+
+export const CustomForm = <T extends FieldValues>({
+  methods,
+  onSubmit,
+  children,
+  className = "",
+  ...rest
+}: Props<T>) => {
+  return (
+    <FormProvider {...methods}>
+      <form
+        {...rest}
+        onSubmit={methods.handleSubmit(onSubmit)}
+        className={clsx("flex flex-col gap-2 w-full", className)}
+      >
+        {children}
+      </form>
+    </FormProvider>
+  );
+};
