3a5b99bd4a56eadd196c81cd0f2187cd7be278f4
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-10-03 11:36:47 -0300

    feat(examples): add WalletInfoCard component

---
+++ examples/src/components/Example/components/WalletInfoCard/WalletInfoCard.tsx
@@ -0,0 +1,39 @@
+import { Address } from "@/components/Address";
+
+interface Props {
+  isConnected: boolean;
+  address: string | null;
+  tokenAddress: string | null;
+  balance: number;
+  balanceError: string | null;
+}
+export const WalletInfoCard: React.FC<Props> = ({
+  address,
+  tokenAddress,
+  balance,
+  balanceError,
+  isConnected,
+}) => {
+  if (!isConnected || !address || !tokenAddress) return null;
+
+  return (
+    <div className="flex flex-col w-full gap-4 items-start p-6 md:p-10 bg-black-bch/75 z-10 rounded-3xl shadow-xl backdrop-blur-xs text-white-bch ">
+      <p className="inline-flex items-center">
+        <span className="mr-1 font-bold text-base sm:text-lg ">Address:</span>{" "}
+        <Address address={address} />
+      </p>
+      <p className="inline-flex items-center">
+        <span className="mr-1 font-bold text-base sm:text-lg ">
+          Token Address:
+        </span>{" "}
+        <Address address={tokenAddress} />
+      </p>
+      <p className="inline-flex items-center">
+        <span className="mr-1 font-bold text-base sm:text-lg ">
+          Balance in sats:
+        </span>{" "}
+        {balanceError ? balanceError : balance}
+      </p>
+    </div>
+  );
+};
