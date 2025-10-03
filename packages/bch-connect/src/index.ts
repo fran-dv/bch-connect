bd3f21617964f9aa492de62d409e31d93e38557a
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-10-03 09:15:29 -0300

    feat: export all the bch-wc2 interfaces

--- packages/bch-connect/src/index.ts
+++ packages/bch-connect/src/index.ts
@@ -4,3 +4,4 @@ export * from "@/hooks/useSignTransaction";
 export * from "@/hooks/useWallet";
 export * from "@/providers/BCHConnectProvider";
 export { type Configuration, createConfig } from "@/config/config";
+export type * from "@bch-wc2/interfaces";
