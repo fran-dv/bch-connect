6bf90fa699d0b44abb97f7cd31777d7eb1429b6f
fran-dv <171869989+fran-dv@users.noreply.github.com>
2025-10-05 13:07:00 -0300

    fix(examples): remove search wallets button from the modal

--- examples/src/main.tsx
+++ examples/src/main.tsx
@@ -14,6 +14,10 @@ const config = createConfig({
     url: "https://github.com/fran-dv/bch-connect",
     icons: ["https://placehold.co/600x400?text=BCHConnect"],
   },
+  modalConfig: {
+    allWallets: "HIDE",
+    themeMode: "dark",
+  },
 });
 
 createRoot(document.getElementById("root")!).render(
